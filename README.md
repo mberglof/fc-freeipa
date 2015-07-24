66da61116e3078927c64d2f04e7c7e003529cf9c
mberglof <marcus.berglof@fundingcircle.com>
2015-07-24 16:48:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# freeipa
+
+Cookbook to setup FreeIPA
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'freeipa'
+maintainer       'Marcus Berglof'
+maintainer_email 'sys@fundingcircle.com'
+license          'BSD 3'
+description      'Installs/Configures freeipa'
+long_description 'Installs/Configures freeipa'
+version          '0.0.1'
+
