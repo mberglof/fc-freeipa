b8c060c6d7c5695ff318abb13910a826b75a069f
mberglof <marcus.berglof@fundingcircle.com>
2016-02-08 17:01:06 +0000

    Get settings from environment instead

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,11 +5,11 @@
 # Copyright (c) 2014 The Authors, All Rights Reserved.
 #
 
-node.set["freeipa"]["ipa_admin_password"] = "ipa_admin_password"
-node.set["freeipa"]["hostname"] = "ipa.fcuat.co.uk"
-node.set["freeipa"]["domain"] = "fcuat.co.uk"
-node.set["freeipa"]["dir_manager_password"] = "dir_manager_password"
-node.set["freeipa"]["realm_name"] = "FCUAT.CO.UK"
+#node.set["freeipa"]["ipa_admin_password"] = "ipa_admin_password"
+#node.set["freeipa"]["hostname"] = "ipa.fcuat.co.uk"
+#node.set["freeipa"]["domain"] = "fcuat.co.uk"
+#node.set["freeipa"]["dir_manager_password"] = "dir_manager_password"
+#node.set["freeipa"]["realm_name"] = "FCUAT.CO.UK"
 
 package "selinux-policy" do
   retries 3
